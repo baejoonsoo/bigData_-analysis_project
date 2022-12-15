c391acd6742c1e87fcb83ad1f09da5ba0c4a4d43
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-12-15 17:10:32 +0900

    change line graph
    - 갯수가 아닌 각 해 전체 장르 수 중의 비율을 값으로 사용
    - 25% 이상 점유한 적 있는 곡만을 line 그래프에 출력

--- oneYearGraph.py
+++ oneYearGraph.py
@@ -6,62 +6,73 @@ end_year = 2021
 
 genreSet = set()
 genreTable = {}
+percentTable = {}
 
 
-def format_age(year):
-    return year // 10 * 10
+def format_age(y):
+    return y // 10 * 10
 
 
 with open('input2.json', encoding="UTF-8") as f:
     json_object = json.load(f)
 
+# 각 년도별 장르 점유율 딕셔너리 생성
 for year in range(start_year, end_year + 1):
+    new_obj_table = {}
     age = format_age(year)
-    key_lst = list(json_object[str(age)][str(year)].keys())
+    percentTable[str(year)] = {}
+    obj = json_object[str(age)]
 
-    genreSet.update(key_lst)
+    for genre in obj[str(year)].keys():
+        if genre in new_obj_table:
+            new_obj_table[genre] += obj[str(year)][genre]
+        else:
+            new_obj_table[genre] = obj[str(year)][genre]
 
+    all_value = sum(new_obj_table.values())
+
+    for key, value in new_obj_table.items():
+        percent = (value / all_value) * 100
+        if percent >= 25:
+            # key_lst = list(percentTable[str(year)].keys())
+            genreSet.update([key])
+        percentTable[str(year)][key] = percent
+
+# genreTable 0으로 초기화
 for genre in list(genreSet):
     genreTable[genre] = [0] * 52
 
+# genreTable에 곡 갯수 저장
 for year in range(start_year, end_year + 1):
-    age = format_age(year)
-
-    obj = json_object[str(age)][str(year)]
+    obj = percentTable[str(year)]
 
     for key in list(obj.keys()):
-        value = obj[key]
-        genreTable[key][year - 1970] += value
+        if key in genreTable:
+            value = obj[key]
+            print(value, year, key)
+            genreTable[key][year - 1970] += value
+    print("----------------------------------------------")
 
+colorTable = ["red", "limegreen", 'violet', 'dodgerblue', 'orange']
 
-genreTable2 = {}
-for genre in genreTable.keys():
-    lst = genreTable[genre]
-    isInsert = False
-
-    for n in lst:
-        if n > 20:
-            isInsert = True
-            break
-    if isInsert:
-        genreTable2[genre] = genreTable[genre]
-
-print(len(genreTable.keys()))
-print(len(genreTable2.keys()))
-
+# line 그래프 생성
+for idx, genre in enumerate(list(genreSet)):
+    if genre in genreTable.keys():
+        plt.plot(genreTable[genre], label=genre, color=colorTable[idx % len(colorTable)])
 
 x = range(0, 53, 10)
 values = range(1970, 2022, 10)
 
-for genre in list(genreSet):
-    if genre in genreTable2.keys():
-        plt.plot(genreTable2[genre])
-
 plt.xlabel('year')
-plt.ylabel('count')
+plt.ylabel('percent')
 plt.xticks(x, values)
 
 plt.grid(True, axis='x', color='gray', alpha=0.5, linestyle="--")
 plt.grid(True, axis='y', color='gray', alpha=0.5, linestyle="--")
 
+plt.rc('font', family='AppleGothic')
+plt.rc('font', family='AppleGothic')
+plt.title('top5 genre')
+
+plt.legend()
 plt.show()
